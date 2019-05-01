3ed5c10fcc6c6a59a6fd6a73930869b8c1593c37
Dominic Saechao <sinchao54@gmail.com>
2019-04-30 23:40:44 -0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let input = document.querySelector("input");
-let result = document.querySelector("#result");
-let button = document.querySelector("#submit_button");
-
-button.addEventListener("click", function () {
-    let value = input.value;
-    console.log(value);
-});
