1cdf53d9caddd9fd5af1c77771cd66c6c1044da8
Dominic Saechao <dominicsaechao1@gmail.com>
2019-05-01 00:18:04 -0700

    Fixed the formula for getting the PI digits. Removed margin from paragraph element.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-let input = document.querySelector("input");
+let input = document.querySelector("#input_Number");
 let result = document.querySelector("#result");
-let button = document.querySelector("#submit_button");
+let button = document.querySelector("#submit_Button");
+const PI = Math.PI;
 
-button.addEventListener("click", function () {
-    let value = input.value;
-    console.log(value);
+
+
+button.addEventListener("click", function (event) {
+    if (input.value >= 13) {
+        document.getElementById("result").innerHTML = "Please enter a number between 1-12";
+    } else {
+        let x = document.getElementById("input_Number").value;
+        document.getElementById("result").innerHTML = Math.PI.toFixed(x - 1);
+    }
 });
